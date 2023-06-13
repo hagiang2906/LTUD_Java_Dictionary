53df9dab17cd7575639367d5818b7f5782c1eb29
hagiang2906 <136446882+hagiang2906@users.noreply.github.com>
2023-06-13 23:18:48 +0700

    Init project

---
+++ dictionary/src/main/java/com/dictionary/Dictionary.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.dictionary;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Dictionary {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
