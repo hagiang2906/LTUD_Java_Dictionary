679d5588b8a321f2b8a1dad2b580f7d62707a25b
hagiang2906 <136446882+hagiang2906@users.noreply.github.com>
2023-06-16 22:05:57 +0700

    add function puase

--- dictionary/src/main/java/com/dictionary/Dictionary.java
+++ dictionary/src/main/java/com/dictionary/Dictionary.java
@@ -3,14 +3,19 @@
  */
 
 package com.dictionary;
-
+import java.util.List;
+import java.util.Scanner;
 /**
  *
  * @author ADMIN
  */
 public class Dictionary {
-
+    static Scanner keyboard = new Scanner(System.in);
+    public static void Pause(){
+        System.out.println("Press Any Key To Continue...");
+        new java.util.Scanner(System.in).nextLine();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
