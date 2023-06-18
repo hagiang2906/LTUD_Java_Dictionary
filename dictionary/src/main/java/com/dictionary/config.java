48265c5a965e68580984a639f08d2d9e57b6c067
hagiang2906 <136446882+hagiang2906@users.noreply.github.com>
2023-06-18 16:31:39 +0700

    Update config.java

--- dictionary/src/main/java/com/dictionary/config.java
+++ dictionary/src/main/java/com/dictionary/config.java
@@ -1,9 +1,9 @@
 package com.dictionary;
 
 public  class config {
-    public static String slangPath="src/data/slang.txt";
-    public static String slangOriginPath ="src/data/slang-origin.txt";
-    public static String historyPath = "src/data/history.txt";
+    public static String slangPath="src/main/java/data/slang.txt";
+    public static String slangOriginPath ="src/main/java/data/slang-origin.txt";
+    public static String historyPath = "src/main/java/data/history.txt";
     public static String getHistoryPath(){
         return historyPath;
     }
